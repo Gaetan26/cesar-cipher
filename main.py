c2b7143730cf0132425f9f43a2955f4e494ecb9a
Gaetan <gaetansg26@gmail.com>
2025-04-03 09:53:33 +0100

    Phase de test terminer !

--- main.py
+++ main.py
@@ -1,3 +1,11 @@
 
 
 from cipher import cipherSentence, decipherSentence
+
+key = 3
+base = "Universite Loyola du Congo"
+
+base_ = cipherSentence(plainText=base, shifKey=key)
+
+print(base, "=>", base_)
+print(base_, "=>", decipherSentence(cypheredText=base_, shifKey=key))
